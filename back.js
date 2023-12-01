e398a0b456d5b9926e538d084ac090e0db64ea77
Nathan <dutierpierre@gmail.com>
2023-12-01 16:22:00 +0100

    Generation cote serveur + mm grille ev1

--- back.js
+++ back.js
@@ -1,9 +1,27 @@
-const express = require('express');
-const { cookie } = require('express/lib/response');
+#!/usr/bin/env node
+
+import * as d3 from "d3";
+import * as jsdom from "jsdom";
+
+import express from "express";
+import pkg from 'express/lib/response.js';
+import * as http from "http";
+import * as socketio from "socket.io";
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
-const http = require('http');
+const { cookie } = pkg;
 const server = http.createServer(app);
-const io = new require("socket.io")(server);
+const io = new socketio.Server(server);
+
+const { JSDOM } = jsdom;
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 server.listen(8880, () => {console.log('Le serveur écoute sur le port 8880');});
 
 let players = [];
@@ -48,13 +66,59 @@ io.on('connection', (socket) => {
         io.emit('messagerie',data); 
     });
 
-    socket.on("fillGrid",hexId => {
-        let tanieres = ["h6","h78","h90","h162"];
-        let color = "";
-        if (tanieres.slice(0,players.length-1).includes(hexId)) {
+    socket.on("createBoard", (nbLignes,nbColonnes,rayHex) => {
+        console.log("Signal createBoard reçu");
+        const svgHTML = createHexagonBoard(nbLignes,nbColonnes,rayHex);
+        io.emit("boardCreated",svgHTML);
+    });
+});
+
+function creeHexagone(rayon){
+    var points = new Array();
+    for (var i = 0; i < 6; ++i){
+    var angle = i*Math.PI / 3;
+    var x = Math.sin(angle)*rayon;
+    var y = -Math.cos(angle)*rayon;
+    points.push([Math.round(x*100)/100, Math.round(y*100)/100]);
+    }
+    return points;
+}
+
+function createHexagonBoard(nbLignes,nbColonnes,rayHex) {
+    const svg = d3.create("svg")
+            .attr("width",Math.sin(Math.PI/3)*rayHex*2*(nbColonnes+(nbLignes-1)/2))
+            .attr("height",rayHex*1.5*nbLignes+rayHex*0.5)
+            .style("background-color","white");
+
+    var offset = Math.sin(Math.PI/3)*rayHex;
+    var  hexagone = creeHexagone(rayHex);
+    for (var l=0; l < nbLignes; l++){
+        for (var c=0;c<nbColonnes;c++){
+            var d = "",x,y;
+            for (var h in hexagone){
+                x = hexagone[h][0]+c*offset*2+offset+l*offset;
+                y = hexagone[h][1]+l*rayHex*1.5+rayHex;
+                if(h==0) d+="M"+x+","+y+" L";
+                else d+= x+","+y+" ";
+            }
+            d+= "Z";
+            svg.append("path")
+                .attr("d",d)
+                .attr("stroke","black")
+                .attr("fill","white")
+                .attr("id","h"+(l*nbLignes+c));
+        }
+    }
+
+    var tanieres = ["h6","h78","h90","h162"];
+
+    for (let i=0; i<=169; i++){
+        var current = svg.attr("id","h"+i.toString());
+        var color = "";
+        if (tanieres.slice(0,players.length).includes(current.attr("id"))) {
             color = "pink";
         } else {
-            rand = Math.floor(Math.random() * 101);
+            var rand = Math.floor(Math.random() * 101);
             if (rand<=14) {
                 color = colors[0];
             } else if (rand>=15 && rand<50) {
@@ -63,8 +127,8 @@ io.on('connection', (socket) => {
                 color = colors[2];
             }
         }
-        //console.log(hexId,color);
-        hexList.push([hexId,color]);
-        io.emit("hexFilled",hexId,color);
-    });
-});
+        hexList.push([current.attr("id"),color]);
+        svg.select("#h"+i.toString()).attr("fill",color);
+    }
+    return svg.node().outerHTML;
+}
